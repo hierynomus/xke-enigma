43fc30ed32a82a1266dde2239943883b95ba9d56
hierynomus <jeroen@hierynomus.com>
2012-02-14 15:44:37 +0100

    Added skeleton project

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "Enigma"
+
+organization := "com.xebialabs"
+
+version := "0.1-SNAPSHOT"
+
+scalaVersion := "2.9.1"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.7.1" % "test"
+
---
+++ src/test/scala/EnigmaSpec.scala
@@ -0,0 +1,20 @@
+import org.scalatest.{Spec, BeforeAndAfter}
+
+class EnigmaSpec extends Spec with BeforeAndAfter {
+
+  var enigma: Enigma = _
+  
+  before {
+    enigma = new Enigma
+  }
+
+  describe("An enigma machine") {
+    it ("should ...") {
+
+    }
+
+    it ("should also ...") {
+
+    }
+  }
+}
